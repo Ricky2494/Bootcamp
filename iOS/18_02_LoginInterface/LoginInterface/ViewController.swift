ec5b59ca85e6203022f3b50b839db46a1f8a563c
Anindya Guha <anindya@Anindyas-MacBook-Pro.local>
2019-02-19 18:16:26 +0530

    LoginModule Session 18_02_2019

---
+++ iOS/18_02_LoginInterface/LoginInterface/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginInterface
+//
+//  Created by Anindya Guha on 18/02/19.
+//  Copyright © 2019 Anindya Guha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
